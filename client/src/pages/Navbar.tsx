248694982df691ea902c54c4f75b83f7f0a81ae6
lakshita <laksita2004@gmail.com>
2024-08-23 00:43:39 +0530

    UI changes in navbar and modified .gitignore

--- client/src/pages/Navbar.tsx
+++ client/src/pages/Navbar.tsx
@@ -4,9 +4,8 @@ import { getUserById } from "../Apis/users";
 import { NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  
+
   const [isOpen, setIsOpen] = useState(false);
-  const [dropOpen, setDropOpen] = useState(false);
   const [showSignout, setShowSignout] = useState(false);
   const userObjGDSC = localStorage.getItem("userObjGDSC");
   const userId = userObjGDSC ? JSON.parse(userObjGDSC).userId : null;
@@ -36,27 +35,20 @@ function Navbar() {
   return (
     <nav
       style={{ zIndex: "10" }}
-      className="sticky top-0 bg-white border-b-2 border-slate-300 p-3 mr-4"
+      className="sticky top-0 bg-transparent backdrop-blur-lg bg-opacity-30 z-50 p-4 shadow-md"
     >
       <div className="block md:hidden">
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400 ml-auto"
+          className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400 ml-auto "
         >
-          <svg
-            className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-          <svg
-            className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-          </svg>
+         <svg
+              className={`fill-current h-6 w-6 transition-transform duration-500 ease-in-out ${isOpen ? 'rotate-90' : ''}`}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d={isOpen ? "M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" : "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"} />
+            </svg>
         </button>
       </div>
       <div
@@ -67,7 +59,7 @@ function Navbar() {
         <div className="hidden md:flex md:items-center">
           <NavLink to="/">
             <img
-              className="gdsc_logo"
+              className="gdsc_logo transition-transform duration-500 transform hover:scale-110"
               src="https://cdn-images-1.medium.com/max/578/1*vZVM7utCuRiZ6-HDsNeYUA@2x.png"
               width="50"
               height="50"
@@ -75,85 +67,59 @@ function Navbar() {
           </NavLink>
         </div>
         <NavLink to="/">
-          <h1 className="text-[#868686] text-lg pl-5">GDSC VNRVJIET.</h1>
+          <h1 className="text-[#868686] text-lg pl-5 transition-all duration-500 ease-in-out hover:text-transparent hover:bg-clip-text  hover:bg-gradient-to-r hover:from-red-500 hover:via-yellow-500 hover:via-green-500 hover:to-blue-500 hover:tracking-wider hover:scale-110">GDSC VNRVJIET.</h1>
         </NavLink>
-        <div className="flex flex-col md:flex-row ml-auto text-center text-slate-600">
-          <div className="relative">
-            <button
-              className="pl-5 pt-1"
-              onClick={() => setDropOpen(!dropOpen)}
-            >
-              About GDSC <span className="text-lg">&#9662;</span>
-            </button>
-            {dropOpen && (
-              <div
-                className="absolute right-0 mt-4 w-max bg-white shadow-lg border-2 border-grey"
-                onBlur={() => setDropOpen(false)}
-              >
-                <NavLink
-                  to="/"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  About
-                </NavLink>
-                <NavLink
-                  to="/leads"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Leads
-                </NavLink>
-                <NavLink
-                  to="/community-guidelines"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Community Guidelines
-                </NavLink>
-                <NavLink
-                  to="/blogs"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Blog
-                </NavLink>
-                <NavLink
-                  to="/solution-challenge"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Solution Challenge
-                </NavLink>
-              </div>
-            )}
-          </div>
-          <button className="pl-5">
-            <NavLink to="/">Home</NavLink>
+        <div className="flex flex-col md:flex-row ml-auto text-center text-slate-600 ">
+        <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white  ">
+          <NavLink to="/" className="relative z-10">Home</NavLink>
+          <span className="absolute inset-0 group-hover:bg-blue-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
+        </button>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/leads" className="relative z-10">Leads</NavLink>
+            <span className="absolute inset-0 group-hover:bg-blue-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
+          </button>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/blogs" className="relative z-10">Blog</NavLink>
+            <span className="absolute inset-0 group-hover:bg-red-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
+          </button>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/solution-challenge" className="relative z-10">Solution Challenge</NavLink>
+            <span className="absolute inset-0 group-hover:bg-red-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
           </button>
           {
             users?.role==="admin" &&
-          <button className="pl-5">
-            <NavLink to="/enter">Evaluate</NavLink>
+            <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+              <NavLink to="/enter" className="relative z-10">Evaluate</NavLink>
+              <span className="absolute inset-0 group-hover:bg-yellow-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
+            </button>
+          }
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/leaderboard" className="relative z-10">Leaderboard</NavLink>
+            <span className="absolute inset-0 group-hover:bg-yellow-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
           </button>
-}
-          <button className="pl-5">
-            <NavLink to="/leaderboard">Leaderboard</NavLink>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/upcoming-events" className="relative z-10">Upcoming Events</NavLink>
+            <span className="absolute inset-0 group-hover:bg-green-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
           </button>
-          <button className="pl-5">
-            <NavLink to="/upcoming-events">Upcoming Events</NavLink>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/past-events" className="relative z-10">Past Events</NavLink>
+            <span className="absolute inset-0 group-hover:bg-green-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
+          </button>  
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/Forum" className="relative z-10">Forum</NavLink>
+            <span className="absolute inset-0 group-hover:bg-blue-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
           </button>
-          <button className="pl-5">
-            <NavLink to="/past-events">Past events</NavLink>
-          </button>
-          <button className="pl-5">
-            <NavLink to="/Forum">Forum</NavLink>
-          </button>
-          <button className="pl-5">
-            <NavLink to="/contact-us">Contact Us</NavLink>
+          <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+            <NavLink to="/contact-us" className="relative z-10">Contact Us</NavLink>
+            <span className="absolute inset-0 group-hover:bg-yellow-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
           </button>
           {userObjGDSC == null ? (
-            <button className="pl-5 relative">
-              <NavLink to="/login">Login</NavLink>
+            <button className="relative px-4 py-2 overflow-hidden rounded-lg group hover:text-white ">
+              <NavLink to="/login" className="relative z-10">Login</NavLink>
+              <span className="absolute inset-0 group-hover:bg-red-500 transform -translate-x-full rounded-lg transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
             </button>
           ) : (
             <div className="relative">
-              {" "}
               <button
                 className="pl-5 pr-10"
                 onClick={() => setShowSignout((prevState) => !prevState)}
