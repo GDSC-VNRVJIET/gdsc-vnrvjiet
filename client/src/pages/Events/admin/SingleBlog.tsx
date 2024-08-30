38de780ecfd0f04c9e81e6fbc4f2b75d4fc2b0aa
srimanikantaBattu <srimanikantabattu04@gmail.com>
2024-08-31 00:46:31 +0530

    Changed the UI of giving access to a person

--- client/src/pages/Events/admin/SingleBlog.tsx
+++ client/src/pages/Events/admin/SingleBlog.tsx
@@ -87,20 +87,60 @@ const SingleBlog: React.FC = () => {
     <div className="px-4 md:px-6 lg:px-8">
       <article className="max-w-3xl mx-auto my-8 p-6 md:p-8 lg:p-10 bg-white shadow-lg rounded-md">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
-          <h2 className="text-2xl md:text-3xl font-bold mb-4 md:mb-0">{blog.title}</h2>
-          {user?.role === "admin" && blog?.show === "true" && (
-            <div className="flex space-x-2 mt-2 md:mt-0">
+          <div className="mx-auto">
+          <h2 className="text-2xl md:text-3xl font-bold mb-4 md:mb-0 text-center">
+            {blog.title}
+          </h2>
+          </div>
+          {user?.role === "admin" && (
+            <div className="inline-flex items-center justify-center rounded-md shadow-sm">
               <button
                 onClick={() => navigate(`/edit-blog/${blogId}`)}
-                className="text-sm md:text-base text-slate-800 hover:text-blue-600 bg-white hover:bg-slate-100 border border-slate-200 rounded-lg font-medium px-3 py-2"
+                className="text-slate-800 hover:text-blue-600 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-l-lg font-medium px-4 py-2 inline-flex space-x-1 items-center"
               >
-                Edit
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-blue-600"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
+                </span>
+                <span className="hidden md:inline-block text-blue-600">
+                  Edit
+                </span>
               </button>
               <button
                 onClick={handleDeleteClick}
-                className="text-sm md:text-base text-slate-800 hover:text-red-600 bg-white hover:bg-slate-100 border border-slate-200 rounded-lg font-medium px-3 py-2"
+                className="text-slate-800 hover:text-blue-600 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-r-lg font-medium px-4 py-2 inline-flex space-x-1 items-center"
               >
-                Delete
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-red-600"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </span>
+                <span className="hidden md:inline-block text-red-600">
+                  Delete
+                </span>
               </button>
             </div>
           )}
