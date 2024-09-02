aae4f80c7ba896107fcc7476e9ca3cc84fa9ce50
jayasree <jgondipalle@gmail.com>
2024-09-02 14:10:13 +0530

    Thank you mail to the author

--- client/src/pages/Events/user/EditBlog.tsx
+++ client/src/pages/Events/user/EditBlog.tsx
@@ -26,20 +26,21 @@ const EditBlog: React.FC = () => {
         if (user?.role === "admin") {
           setDisplayLoader(false);
           setAccess(true);
-        }
-        else{
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACK_URL}/addblog/getAccessdata/${user?.emailId}`
-        );
-        console.log(response.data.success);
-        if (response.data.success === true) {
-          if (id === response.data.payload.blogId) {
-            setAccess(true);
-            setDisplayLoader(false);
+        } else {
+          const response = await axios.get(
+            `${process.env.REACT_APP_BACK_URL}/addblog/getAccessdata/${user?.emailId}`
+          );
+          console.log(response.data.success);
+          if (response.data.success === true) {
+            if (id === response.data.payload.blogId) {
+              setAccess(true);
+              setDisplayLoader(false);
+            }
           }
         }
+      } catch (error) {
+        console.log(error);
       }
-      } catch (error) {console.log(error);}
     };
     getUserData();
   }, []);
@@ -52,7 +53,7 @@ const EditBlog: React.FC = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [thumbnail, setThumbnail] = useState<string>("");
   const [base64Image, setBase64Image] = useState<string>("");
-  const [author,setAuthor] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
 
   // get local storage details
   const [user, setUser] = useState(
@@ -64,12 +65,15 @@ const EditBlog: React.FC = () => {
   );
 
   async function sendEmailToAdmin() {
-    if(user?.emailId!=="admin@gmail.com"){
+    if (user?.emailId !== "admin@gmail.com") {
       // send mail to gdsc.vnrvjiet@gmail.com
-      await axios.post(`${process.env.REACT_APP_BACK_URL}/sendmail/notify-admin`, {
-        email: user?.emailId,
-        blogId: id,
-      })
+      await axios.post(
+        `${process.env.REACT_APP_BACK_URL}/sendmail/notify-admin`,
+        {
+          email: user?.emailId,
+          blogId: id,
+        }
+      );
     }
   }
   useEffect(() => {
@@ -149,10 +153,10 @@ const EditBlog: React.FC = () => {
         // { withCredentials: true, headers: { Authorization: `Bearer ${user?.token}` } }
       );
 
-      // console.log("Response from backend:", response.data.message);
-      console.log(response);
       if (response.data.message === "Blog updated successfully") {
-        alert("Blog updated successfully!");
+        alert(
+          "Your blog has been successfully updated! We've notified our admin, and you'll receive an email once your blog is published. Thank you!"
+        );
         navigate(`/blogs/${id}`);
       }
     } catch (error) {
@@ -164,78 +168,80 @@ const EditBlog: React.FC = () => {
     <Loader />
   ) : blog && access ? (
     <div className="container mx-auto p-4 max-w-4xl">
-  <h2 className="text-3xl font-bold mb-6 text-center">Edit Blog</h2>
-  <form className="space-y-6">
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-      <input
-        type="text"
-        value={title}
-        onChange={handleTitleChange}
-        placeholder="Title"
-        className="w-full p-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
-      />
-      <input
-        type="text"
-        value={author}
-        onChange={handleAuthorChange}
-        placeholder="Author"
-        className="w-full p-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
-      />
-    </div>
-    <div className="space-y-4">
-      {categories.map((category, index) => (
-        <div key={index} className="flex items-center gap-4">
+      <h2 className="text-3xl font-bold mb-6 text-center">Edit Blog</h2>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           <input
             type="text"
-            value={category}
-            onChange={(e) => handleCategoryChange(index, e.target.value)}
-            placeholder="Category"
+            value={title}
+            onChange={handleTitleChange}
+            placeholder="Title"
             className="w-full p-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
           />
+          <input
+            type="text"
+            value={author}
+            onChange={handleAuthorChange}
+            placeholder="Author"
+            className="w-full p-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
+          />
+        </div>
+        <div className="space-y-4">
+          {categories.map((category, index) => (
+            <div key={index} className="flex items-center gap-4">
+              <input
+                type="text"
+                value={category}
+                onChange={(e) => handleCategoryChange(index, e.target.value)}
+                placeholder="Category"
+                className="w-full p-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
+              />
+              <button
+                type="button"
+                onClick={() => removeCategoryField(index)}
+                className="bg-red-500 text-white px-3 py-2 rounded-lg"
+              >
+                &times;
+              </button>
+            </div>
+          ))}
+          {categories.length < 3 && (
+            <button
+              type="button"
+              onClick={addCategoryField}
+              className="bg-green-500 text-white py-2 px-4 rounded-lg"
+            >
+              + Add Category
+            </button>
+          )}
+        </div>
+        <div className="mb-4">
+          <ReactQuill
+            value={description}
+            onChange={setDescription}
+            className="border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
+            style={{
+              height: "300px",
+              maxHeight: "500px",
+              width: "100%",
+              overflow: "auto",
+            }}
+          />
+        </div>
+        <div className="flex justify-center">
           <button
             type="button"
-            onClick={() => removeCategoryField(index)}
-            className="bg-red-500 text-white px-3 py-2 rounded-lg"
+            onClick={() => {
+              handleUpdateBlog();
+              sendEmailToAdmin();
+            }}
+            className="bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-700"
           >
-            &times;
+            Update Blog
           </button>
         </div>
-      ))}
-      {categories.length < 3 && (
-        <button
-          type="button"
-          onClick={addCategoryField}
-          className="bg-green-500 text-white py-2 px-4 rounded-lg"
-        >
-          + Add Category
-        </button>
-      )}
+      </form>
     </div>
-    <div className="mb-4">
-      <ReactQuill
-        value={description}
-        onChange={setDescription}
-        className="border rounded-lg focus:outline-none focus:ring focus:border-blue-50"
-        style={{
-          height: "300px",
-          maxHeight: "500px",
-          width: "100%",
-          overflow: "auto",
-        }}
-      />
-    </div>
-    <div className="flex justify-center">
-      <button
-        type="button"
-        onClick={() => { handleUpdateBlog(); sendEmailToAdmin(); }}
-        className="bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-700"
-      >
-        Update Blog
-      </button>
-    </div>
-  </form>
-</div>
-
   ) : (
     <div className="flex justify-center items-center min-h-screen">
       <p className="text-red-500 text-6xl">Access Denied</p>
