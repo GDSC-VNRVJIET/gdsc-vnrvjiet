f2435d7e9d7c499f9e6f795eaad69a6f45f8dfe9
srimanikantaBattu <srimanikantabattu04@gmail.com>
2024-10-12 21:34:22 +0530

    Changes while Scanning

--- client/src/pages/Events/admin/CheckUsers.tsx
+++ client/src/pages/Events/admin/CheckUsers.tsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
+import Verified from "../../../images/verifiedAnimation.gif"
+import Mistake from "../../../images/mistake-5026_256.gif"
+import DontAllow from "../../../images/sign.png"
 import axios from "axios";
 import { Html5QrcodeScanner } from "html5-qrcode";
 function CheckUsers() {
   const [scanResult, setScanResult] = useState<string | null>(null);
   const [msg, setMsg] = useState<string>("");
   const [data, setData] = useState<string>("");
+  const [data1,setData1] = useState<any>("");
   const [eventname, setEventname] = useState<string>("");
   const [loading,setLoading] = useState(false);
+  const [button,showButton] = useState(true);
   useEffect(() => {
     const scanner = new Html5QrcodeScanner(
       "reader",
@@ -39,9 +44,11 @@ function CheckUsers() {
       `${process.env.REACT_APP_BACK_URL}/registration/register`,
       posted
     );
-    setMsg(JSON.stringify(res.data.message));
-    setData(JSON.stringify(res.data.payload.name));
-    setEventname(JSON.stringify(res.data.payload.event));
+    setMsg(res.data.message);
+    setData(res.data.payload.name);
+    setEventname(res.data.payload.event);
+    setData1(res.data.payload);
+    showButton(false);
     setLoading(false);
   }
   if(loading) return <p className="text-center block mx-auto">Loading...</p>
@@ -58,9 +65,14 @@ function CheckUsers() {
               Check
             </button>
           </div>
-          <p className="text-center mt-9">{msg} </p>{" "}
-          <p className="block text-center mt-4"> {data} </p>
-          <p className="block text-center mt-4"> {eventname} </p>
+          <p className="text-center mt-9">{msg} </p>
+          {
+            msg==="Allow To Workshop" ? <img src={Verified} alt="Verified" className="block mx-auto"/> : (
+              msg==="Already Scanned Dont Allow to Workshop" ? <img src={DontAllow} alt="Already Scanned" /> : <img src={Mistake} alt="Invalid QR" />
+            ) 
+          }
+          <p className="block text-center mt-4">{data}  {data1.rollno}</p>
+          <p className="block text-center mt-4">{eventname}</p>
         </div>
       ) : (
         <div id="reader" style={{marginTop:"200px"}}></div>
