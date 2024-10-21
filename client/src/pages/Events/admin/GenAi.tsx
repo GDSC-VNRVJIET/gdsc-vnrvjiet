c3e31fac40350ccf49a213b502697080fda2e2ea
jayasree <jgondipalle@gmail.com>
2024-10-21 12:15:44 +0530

    gen ai leaderboard modifications

--- client/src/pages/Events/admin/GenAi.tsx
+++ client/src/pages/Events/admin/GenAi.tsx
@@ -21,7 +21,10 @@ function GenAi() {
         );
         const sortedData = res.data.data.sort((a: any, b: any) => {
           return (
-            b["# of Skill Badges Completed"] - a["# of Skill Badges Completed"]
+            b["# of Skill Badges Completed"] +
+            b["# of Arcade Games Completed"] -
+            (a["# of Skill Badges Completed"] +
+              a["# of Arcade Games Completed"])
           );
         });
         setExcelData(sortedData);
@@ -68,7 +71,9 @@ function GenAi() {
 
       const sortedData = data.sort((a: any, b: any) => {
         return (
-          b["# of Skill Badges Completed"] - a["# of Skill Badges Completed"]
+          b["# of Skill Badges Completed"] +
+          b["# of Arcade Games Completed"] -
+          (a["# of Skill Badges Completed"] + a["# of Arcade Games Completed"])
         );
       });
 
@@ -90,79 +95,136 @@ function GenAi() {
 
   return (
     <div className="wrapper p-8 bg-gray-100 min-h-screen">
-      {
-        user && user.role === process.env.REACT_APP_ADMIN_ROLE &&
-        (
-      <div className="">
-      <h3 className="text-2xl font-bold text-gray-700 mb-6 text-center">
-        Upload & View Excel Sheets
-      </h3>
+      {user && user.role === process.env.REACT_APP_ADMIN_ROLE && (
+        <div className="">
+          <h3 className="text-2xl font-bold text-gray-700 mb-6 text-center">
+            Upload & View Excel Sheets
+          </h3>
 
-      {/* form */}
-      <form
-        className="form-group custom-form mb-6 flex flex-col items-center"
-        onSubmit={handleUpdateSubmit}
-      >
-        <input
-          type="file"
-          className="form-control p-3 border rounded-md w-full max-w-md mb-4 bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-          onChange={handleFile}
-        />
-        <button
-          type="submit"
-          className="btn btn-success bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-md"
-        >
-          UPDATE
-        </button>
-        {typeError && (
-          <div className="alert alert-danger text-red-600 mt-4">
-            {typeError}
-          </div>
-        )}
-      </form>
-      </div>
-        )
-}
+          {/* form */}
+          <form
+            className="form-group custom-form mb-6 flex flex-col items-center"
+            onSubmit={handleUpdateSubmit}
+          >
+            <input
+              type="file"
+              className="form-control p-3 border rounded-md w-full max-w-md mb-4 bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+              onChange={handleFile}
+            />
+            <button
+              type="submit"
+              className="btn btn-success bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-md"
+            >
+              UPDATE
+            </button>
+            {typeError && (
+              <div className="alert alert-danger text-red-600 mt-4">
+                {typeError}
+              </div>
+            )}
+          </form>
+        </div>
+      )}
       {/* view data */}
       <div className="viewer">
-      <h1 className="text-4xl text-center mb-4 font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-400 to-blue-400">
-  GEN AI STUDY JAMS LEADERBOARD
-</h1>
+        <h1 className="text-4xl text-center mb-4 font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-400 to-blue-400">
+          GEN AI STUDY JAMS LEADERBOARD
+        </h1>
 
-  {excelData ? (
-    <div className="w-full max-w-6xl mx-auto overflow-x-auto"> {/* Added overflow-x-auto */}
-      <table className="table-auto min-w-full text-left border-collapse bg-white shadow-md rounded-lg">
-        <thead>
-          <tr className="bg-gradient-to-r from-blue-500 to-green-500 text-white">
-            <th className="py-3 px-6 text-sm font-semibold uppercase">Rank</th>
-            <th className="py-3 px-6 text-sm font-semibold uppercase">Name</th>
-            <th className="py-3 px-6 text-sm font-semibold uppercase">Email</th>
-            <th className="py-3 px-6 text-sm font-semibold uppercase">Skill Badges Completed</th>
-            <th className="py-3 px-6 text-sm font-semibold uppercase">Names of Completed Skill Badges</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-700">
-          {excelData.map((individualExcelData, index) => (
-            <tr 
-              key={index} 
-              className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-200`}
-            >
-              <td className="py-3 px-6 border-b">{index + 1}</td> {/* S.No */}
-              <td className="py-3 px-6 border-b">{individualExcelData['User Name']}</td> {/* User Name */}
-              <td className="py-3 px-6 border-b">{individualExcelData['User Email']}</td> {/* User Email */}
-              <td className="py-3 px-6 border-b">{individualExcelData['# of Skill Badges Completed']}</td> {/* # of Badges */}
-              <td className="py-3 px-6 border-b">{individualExcelData['Names of Completed Skill Badges']}</td> {/* Skill Badges */}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  ) : (
-    <div className="text-center text-gray-500">Loading...</div>
-  )}
-</div>
-
+        {excelData ? (
+          <div className="w-full max-w-6xl mx-auto overflow-x-auto">
+            {" "}
+            {/* Added overflow-x-auto */}
+            <table className="table-auto min-w-full text-left border-collapse bg-white shadow-md rounded-lg">
+              <thead>
+                <tr className="bg-gradient-to-r from-blue-500 to-green-500 text-white">
+                  <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Rank
+                  </th>
+                  <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Name
+                  </th>
+                  <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Google Cloud Skills Boost Profile URL
+                  </th>
+                  <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Skill Badges Completed
+                  </th>
+                  {/* <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Names of Completed Skill Badges
+                  </th> */}
+                  <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Arcade Games Completed
+                  </th>
+                  {/* <th className="py-3 px-6 text-sm font-semibold uppercase">
+                    Names of Completed Arcade Badges
+                  </th> */}
+                </tr>
+              </thead>
+              <tbody className="text-gray-700">
+                {excelData.map((individualExcelData, index) => (
+                  <tr
+                    key={index}
+                    className={`${
+                      index % 2 === 0 ? "bg-gray-100" : "bg-white"
+                    } hover:bg-gray-200`}
+                  >
+                    <td className="py-3 px-6 border-b">{index + 1}</td>{" "}
+                    {/* S.No */}
+                    <td className="py-3 px-6 border-b">
+                      {individualExcelData["User Name"]}
+                    </td>
+                    {/* User Name */}
+                    {/* <td className="py-3 px-6 border-b">{individualExcelData['User Email']}</td> User Email */}
+                    <td className="py-3 px-6 border-b">
+                      {individualExcelData[
+                        "Google Cloud Skills Boost Profile URL"
+                      ] && (
+                        <a
+                          href={
+                            individualExcelData[
+                              "Google Cloud Skills Boost Profile URL"
+                            ]
+                          }
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 hover:underline"
+                        >
+                          {
+                            individualExcelData[
+                              "Google Cloud Skills Boost Profile URL"
+                            ]
+                          }
+                        </a>
+                      )}
+                    </td>
+                    {/* Profile URL */}
+                    <td className="py-3 px-6 border-b">
+                      {individualExcelData["# of Skill Badges Completed"]}
+                    </td>
+                    {/* # of Badges */}
+                    {/* <td className="py-3 px-6 border-b">
+                      {individualExcelData["Names of Completed Skill Badges"]}
+                    </td> */}
+                    {/* Skill Badges */}
+                    <td className="py-3 px-6 border-b">
+                      {individualExcelData["# of Arcade Games Completed"]}
+                    </td>
+                    {/* # of Arcade Badges */}
+                    {/* <td className="py-3 px-6 border-b">
+                      {individualExcelData["Names of Completed Arcade Games"]}
+                    </td> */}
+                    {/* Arcade Badges */}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="text-center text-gray-500">Loading...</div>
+        )}
+      </div>
     </div>
   );
 }
