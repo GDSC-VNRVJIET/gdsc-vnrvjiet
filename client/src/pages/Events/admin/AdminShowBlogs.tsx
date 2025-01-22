13996a4efd7a71db263c3c9bb6789512b85d1dd5
lakshita <laksita2004@gmail.com>
2025-01-22 20:12:56 +0530

    Minor blogs fix

--- client/src/pages/Events/admin/AdminShowBlogs.tsx
+++ client/src/pages/Events/admin/AdminShowBlogs.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import CurrentBlogs from "./CurrentBlogs";
 import Loader from "../../Loader";
 
-
 interface Blog {
   author?: string;
   thumbnail?: string;
@@ -300,35 +299,45 @@ const AdminShowBlogs: React.FC = () => {
                 </>
               ) : (
                 <>
-                <div className="flex flex-col gap-4">
-                  <input
-                    type="text"
-                    value={blogTitle}
-                    onChange={handleTitleChange}
-                    required
-                    placeholder="Title"
-                    className="p-2 border border-gray-300 rounded"
-                  />
-                  <select
-                    value={domain}
-                    onChange={handleDomainChange}
-                    required
-                    className="p-2 border border-gray-300 rounded"
-                    defaultValue={""}
-                  >
-                    <option value="" disabled>Select Domain</option>
-                    <option value="Web-Development">Web Development</option>
-                    <option value="Competitive Programming">Competitive Programming</option>
-                    <option value="Artificial Intelligence and Machine Learning">Artificial Intelligence and Machine Learning</option>
-                    <option value="Management">Management</option>
-                    <option value="Social Media and Design">Social Media and Design</option>
-                    <option value="App Development">App Development</option>
-                    <option value="Hardware">Hardware</option>
-                    <option value="Women-in-Tech">Women in Tech</option>
-                    <option value="Testing and Cyber Security">Testing and Cyber Security</option>
-                  </select>
-                </div>
-                <div className="flex flex-col items-center">
+                  <div className="flex flex-col gap-4">
+                    <input
+                      type="text"
+                      value={blogTitle}
+                      onChange={handleTitleChange}
+                      required
+                      placeholder="Title"
+                      className="p-2 border border-gray-300 rounded"
+                    />
+                    <select
+                      value={domain}
+                      onChange={handleDomainChange}
+                      required
+                      className="p-2 border border-gray-300 rounded"
+                      defaultValue={""}
+                    >
+                      <option value="" disabled>
+                        Select Domain
+                      </option>
+                      <option value="Web-Development">Web Development</option>
+                      <option value="Competitive Programming">
+                        Competitive Programming
+                      </option>
+                      <option value="Artificial Intelligence and Machine Learning">
+                        Artificial Intelligence and Machine Learning
+                      </option>
+                      <option value="Management">Management</option>
+                      <option value="Social Media and Design">
+                        Social Media and Design
+                      </option>
+                      <option value="App Development">App Development</option>
+                      <option value="Hardware">Hardware</option>
+                      <option value="Women-in-Tech">Women in Tech</option>
+                      <option value="Testing and Cyber Security">
+                        Testing and Cyber Security
+                      </option>
+                    </select>
+                  </div>
+                  <div className="flex flex-col items-center">
                     {fileName ? (
                       <div className="flex items-center justify-between w-full p-4 border-dashed border-2 border-gray-500 rounded">
                         <span className="text-gray-700">{fileName}</span>
@@ -431,94 +440,113 @@ const AdminShowBlogs: React.FC = () => {
         {activeTab === "tab1" ? (
           <div className="relative mt-3 lg:mx-8">
             <ul className="mt-10">
-          {blogs.length > 0 ? (
-            blogs.map((blog) => (
-              <section
-                key={blog._id}
-                className="flex flex-col justify-center antialiased mb-20"
-              >
-                <div className="max-w-6xl mx-auto px-8 py-2">
-                  <article
-                    className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center cursor-pointer transition-all duration-300 ease-out hover:-translate-y-1 group"
-                    onClick={() => handleBlogClick(blog._id)}
+              {blogs.length > 0 ? (
+                blogs.map((blog) => (
+                  <section
+                    key={blog._id}
+                    className="flex flex-col justify-center antialiased mb-20"
                   >
-                    <div className="relative block group-hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl p-2">
-                      <div
-                        className="absolute inset-0 bg-gray-200 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none rounded-xl"
-                        aria-hidden="true"
-                      ></div>
-                      <figure className="relative h-[400px] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out rounded-xl">
-                        <img
-                          className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out rounded-xl"
-                          src={blog.thumbnail}
-                          width="540"
-                          height="303"
-                          alt="Blog post"
-                        />
-                      </figure>
-                    </div>
-                    <div className="flex flex-col h-[400px] justify-between group-hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl p-6">
-                      <header>
-                        <h3 className="text-2xl lg:text-3xl font-bold leading-tight mb-3">
-                          <a className="hover:text-blue-500 transition duration-150 ease-in-out">
-                            {blog.title}
-                          </a>
-                        </h3>
-                      </header>
-                      <div className="flex-grow overflow-hidden mb-4">
-                        <div
-                          className="text-gray-600 text-lg prose max-w-none line-clamp-6"
-                          dangerouslySetInnerHTML={{
-                            __html: blog.description,
-                          }}
-                        />
-                      </div>
-                      {blog.isCommunity === "false" && (
-                        <div>
-                          <div className="mb-3 flex flex-wrap font-medium text-sm -m-1">
-                            {(blog.category || "").split(",").map((cat, idx) => (
-                              <span key={idx} className="m-1">
-                                <span
-                                  className={`inline-flex text-center text-gray-100 py-2 px-4 rounded-full transition duration-150 ease-in-out ${
-                                    colors[idx % colors.length]
-                                  }`}
-                                >
-                                  {cat.trim()}
-                                </span>
-                              </span>
-                            ))}
-                          </div>
-                          <footer className="flex items-center mt-4">
-                            <a>
-                              <img
-                                className="rounded-full flex-shrink-0 mr-4"
-                                src="https://tse2.mm.bing.net/th?id=OIP.XadmtOiEEI6Zv388n5l2dQHaHx&pid=Api&P=0&h=220"
-                                width="40"
-                                height="40"
-                                alt="Author"
-                              />
-                            </a>
-                            <div>
-                              <a className="font-medium text-gray-800 hover:text-green-400 transition duration-150 ease-in-out">
-                                {blog.author}
-                              </a>
-                              <span className="text-gray-700"> - </span>
-                              <span className="text-gray-500">
-                                {blog.date}
-                              </span>
-                            </div>
-                          </footer>
+                    <div className="max-w-6xl mx-auto px-8 py-2">
+                      <article className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center cursor-pointer transition-all duration-300 ease-out hover:-translate-y-1 group">
+                        <div className="relative block group-hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl p-2">
+                          <div
+                            className="absolute inset-0 bg-gray-200 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none rounded-xl"
+                            aria-hidden="true"
+                          ></div>
+                          <figure
+                            className="relative h-[400px] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out rounded-xl"
+                            onClick={() => handleBlogClick(blog._id)}
+                          >
+                            <img
+                              className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out rounded-xl"
+                              src={blog.thumbnail}
+                              width="540"
+                              height="303"
+                              alt="Blog post"
+                            />
+                          </figure>
                         </div>
-                      )}
+                        <div className="flex flex-col h-[400px] justify-between group-hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl p-6">
+                          <header className="flex justify-between">
+                            <h3
+                              className="text-2xl lg:text-3xl font-bold leading-tight mb-3"
+                              onClick={() => handleBlogClick(blog._id)}
+                            >
+                              <a className="hover:text-blue-500 transition duration-150 ease-in-out">
+                                {blog.title}
+                              </a>
+                            </h3>
+                            {showRadioButtons && (
+                              <input
+                                type="radio"
+                                name="selectedBlog"
+                                value={blog._id}
+                                onChange={() => handleBlogSelection(blog._id)}
+                                className="text-right z-10 border-8 border-blue-600"
+                                style={{ width: "20px", height: "20px" }}
+                              />
+                            )}
+                          </header>
+                          <div
+                            className="flex-grow overflow-hidden mb-4"
+                            onClick={() => handleBlogClick(blog._id)}
+                          >
+                            <div
+                              className="text-gray-600 text-lg prose max-w-none line-clamp-6"
+                              dangerouslySetInnerHTML={{
+                                __html: blog.description,
+                              }}
+                            />
+                          </div>
+
+                          {blog.isCommunity === "false" && (
+                            <div>
+                              <div className="mb-3 flex flex-wrap font-medium text-sm -m-1">
+                                {(blog.category || "")
+                                  .split(",")
+                                  .map((cat, idx) => (
+                                    <span key={idx} className="m-1">
+                                      <span
+                                        className={`inline-flex text-center text-gray-100 py-2 px-4 rounded-full transition duration-150 ease-in-out ${
+                                          colors[idx % colors.length]
+                                        }`}
+                                      >
+                                        {cat.trim()}
+                                      </span>
+                                    </span>
+                                  ))}
+                              </div>
+                              <footer className="flex items-center mt-4">
+                                <a>
+                                  <img
+                                    className="rounded-full flex-shrink-0 mr-4"
+                                    src="https://tse2.mm.bing.net/th?id=OIP.XadmtOiEEI6Zv388n5l2dQHaHx&pid=Api&P=0&h=220"
+                                    width="40"
+                                    height="40"
+                                    alt="Author"
+                                  />
+                                </a>
+                                <div>
+                                  <a className="font-medium text-gray-800 hover:text-green-400 transition duration-150 ease-in-out">
+                                    {blog.author}
+                                  </a>
+                                  <span className="text-gray-700"> - </span>
+                                  <span className="text-gray-500">
+                                    {blog.date}
+                                  </span>
+                                </div>
+                              </footer>
+                            </div>
+                          )}
+                        </div>
+                      </article>
                     </div>
-                  </article>
-                </div>
-              </section>
-            ))
-          ) : (
-            <p>No blogs available.</p>
-          )}
-        </ul>
+                  </section>
+                ))
+              ) : (
+                <p>No blogs available.</p>
+              )}
+            </ul>
           </div>
         ) : (
           <CurrentBlogs />
