c8468d71e5ca521fc0a5d6b69e6c6492d59aa8fe
GDSC-VNRVJIET <srimanikantabattu04@gmail.com>
2025-03-19 18:31:08 +0530

    Slight changes

--- client/src/pages/GoogleForms.tsx
+++ client/src/pages/GoogleForms.tsx
@@ -17,7 +17,7 @@ const TabComponent: React.FC = () => {
               <img
                 src={Gen}
                 alt="Coordinator"
-                className="w-full lg:w-[546px] lg:pr-3 mt-1 rounded mx-auto"
+                className="w-full lg:w-[646px] lg:h-[350px] lg:pr-3 mt-1 rounded mx-auto" // Dont use height next tym !!
               />
             </div>
             {isLoading && (
