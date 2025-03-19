676cff5ccdea5d52d338d706ee14ec9fb0b845a1
GDSC-VNRVJIET <srimanikantabattu04@gmail.com>
2025-03-19 18:17:04 +0530

    Added Flutter Event

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -164,7 +164,8 @@ function Root() {
             <Route path="/admin-login" element={<Login />} /> */}
         <Route path="/signup" element={<Signup />} />
         {/* <Route path="upcoming-events/:eventname" element={<Register />} /> */}
-        <Route path="/upcoming-events/Solution Challenge 2025" element={<GoogleForms />} />
+        {/* <Route path="/upcoming-events/Solution Challenge 2025" element={<GoogleForms />} /> */}
+        <Route path="/flutterflow-register" element={<GoogleForms />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/blogs" element={<Blog />}>
--- client/src/pages/GoogleForms.tsx
+++ client/src/pages/GoogleForms.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import Gen from "../images/sc.png";
+import Gen from "../images/Flutter.png";
 
 const TabComponent: React.FC = () => {
 
@@ -10,41 +10,41 @@ const TabComponent: React.FC = () => {
   };
 
   return (
-    // <div className="w-full md:px-0">
-    //   <div className="content">
-    //       <div className="relative h-[700px] overflow-auto scrollbar-hide bg-green-200">
-    //         <div className="relative">
-    //           <img
-    //             src={Gen}
-    //             alt="Coordinator"
-    //             className="w-full lg:w-[646px] lg:pr-3 mt-1 rounded mx-auto"
-    //           />
-    //         </div>
-    //         {isLoading && (
-    //             <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white z-10">
-    //                 <p className="text-lg font-semibold">Loading...</p>
-    //             </div>
-    //         )}
-    //         <iframe
-    //           src="https://docs.google.com/forms/d/e/1FAIpQLSeMOw5PDbM7uXx9YIaDtl0v-jK5Hm-mBhiytshpmGYYqEMhiA/viewform?embedded=true"
-    //           className="w-full h-full relative z-0 mt-[10px]"
-    //           style={{ border: 0 }}
-    //           allowFullScreen
-    //           onLoad={handleIframeLoad}
-    //           title="Third Year Google Form"
-    //           id="gform"
-    //         >
-    //           Loading…
-    //         </iframe>
-    //       </div>
-    //   </div>
+    <div className="w-full md:px-0">
+      <div className="content">
+          <div className="relative h-[700px] overflow-auto scrollbar-hide bg-blue-200">
+            <div className="relative">
+              <img
+                src={Gen}
+                alt="Coordinator"
+                className="w-full lg:w-[546px] lg:pr-3 mt-1 rounded mx-auto"
+              />
+            </div>
+            {isLoading && (
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white z-10">
+                    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-2 border-blue-500"></div>
+                </div>
+            )}
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeM5EvuRgPU-OUuOZdQYAs-W660eI_eQU11K7CqvB4hlOeHpg/viewform?embedded=true"
+              className="w-full h-full relative z-0 mt-[10px]"
+              style={{ border: 0 }}
+              allowFullScreen
+              onLoad={handleIframeLoad}
+              title="Third Year Google Form"
+              id="gform"
+            >
+              Loading…
+            </iframe>
+          </div>
+      </div>
+    </div>
+    // <div className="flex justify-center items-center mt-[12rem]">
+    //   <div className="bg-red-500 text-white p-4 rounded-lg shadow-md text-center">
+    //   <p className="text-lg font-bold">The form is now closed!</p>
+    //   <p className="text-sm mt-2">Thank you for your interest. Stay tuned for future opportunities!</p>
     // </div>
-    <div className="flex justify-center items-center mt-[12rem]">
-      <div className="bg-red-500 text-white p-4 rounded-lg shadow-md text-center">
-      <p className="text-lg font-bold">The form is now closed!</p>
-      <p className="text-sm mt-2">Thank you for your interest. Stay tuned for future opportunities!</p>
-    </div>
-    </div>
+    // </div>
   );
 };
 
