fc218c79504e8559d49affaadaa05c802e1e3fed
Lakshita Goyal <laksita2004@gmail.com>
2025-03-23 11:03:06 +0530

    Minor changes in blockchain workshop

--- client/src/pages/Events/user/Register.tsx
+++ client/src/pages/Events/user/Register.tsx
@@ -122,7 +122,7 @@ const PaymentGatewayRazorpay: React.FC = () => {
     }
   }, [isValid, formFields]);
   const paymentHandler = async (formdata: FormData) => {
-    const amount = 100.0;
+    const amount = 4900.0;
     const currency = "INR";
     const receiptId = "1235823";
     const check = await axios.post(
@@ -486,11 +486,11 @@ const PaymentGatewayRazorpay: React.FC = () => {
             className="w-full mx-auto border bg-gray-50 p-4"
           >
             <div className="w-full mt-3 lg:ml-3 flex flex-wrap space-x-1.5">
-              <div className="relative mt-6 mb-8 w-full lg:w-[45%] lg:ml-1.5 min-w-[200px] h-10">
+              <div className="relative mt-6 mb-8 ms-2 lg:ms-0 w-full lg:w-[45%] lg:ml-1.5 min-w-[200px] h-10">
                 <input
                   type="text"
                   {...register("name", { required: true })}
-                  className="peer bg-white w-full h-full bg-transparent text-blue-gray-700 font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+                  className="peer bg-white w-full h-full  bg-transparent text-blue-gray-700 font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
                   placeholder=" "
                 />
                 <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
@@ -643,7 +643,7 @@ const PaymentGatewayRazorpay: React.FC = () => {
               </div>
               <div className="w-full lg:w-11/12 mx-auto mt-6">
                 <label htmlFor="interest" className="ms-1 text-gray-500">
-                What interests you to join the challenge?
+                What interests you to join the event?
                 </label>
                 <textarea
                   {...register("interest")}
